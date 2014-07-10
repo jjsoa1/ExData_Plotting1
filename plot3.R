7394f7e609c09c012dcf19f8bee78be30ef49eff
Jan Soliman <jansoliman@anna-solimans-macbook.local>
2014-07-09 20:49:47 -0700

    Add fixed graph. Also change variable names to be more semantic.

--- plot3.R
+++ plot3.R
@@ -3,9 +3,11 @@ source('read.R')
 energy_sub_metering <- function() {
   data <- read_data("household_power_consumption.txt")
   png(file='plot3.png', width=480, height=480, units='px')
+  legend <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
   plot(data[['Calendar_Time']], data[['Sub_metering_1']], type="l", xlab="", ylab="Energy sub metering")
   lines(data[['Calendar_Time']], data[['Sub_metering_2']], type="l", col="red")
   lines(data[['Calendar_Time']], data[['Sub_metering_3']], type="l", col="blue")
+  legend(x='topright', lty=1, lwd=1, col=c('black', 'blue', 'red'), legend=legend)
   dev.off()
 }
 
--- plot4.R
+++ plot4.R
@@ -6,11 +6,11 @@ multiple_plots <- function() {
   par (mfrow = c(2,2))
   plot(data[['Calendar_Time']], data[['Global_active_power']], xlab="", ylab="Global Active Power", type="l")
   plot(data[['Calendar_Time']], data[['Voltage']], xlab="datetime", ylab="Voltage", type="l")
-  cols = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+  legend <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
   plot(data[['Calendar_Time']], data[['Sub_metering_1']], type="l", xlab="", ylab="Energy sub metering")
   lines(data[['Calendar_Time']], data[['Sub_metering_2']], type="l", col="red")
   lines(data[['Calendar_Time']], data[['Sub_metering_3']], type="l", col="blue")
-  legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=cols, bty="n")
+  legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=legend, bty="n")
   plot(data[['Calendar_Time']], data[['Global_reactive_power']], xlab="datetime", ylab="Global_reactive_power", type="l")
   dev.off()
 }
