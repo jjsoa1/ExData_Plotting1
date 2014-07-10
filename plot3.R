ccfab78985d5987c11f07f1037b6fb1de2436ca5
Jan Soliman <jansoliman@anna-solimans-macbook.local>
2014-07-09 21:03:05 -0700

    Whitespace issue.

--- plot3.R
+++ plot3.R
@@ -5,6 +5,7 @@ energy_sub_metering <- function() {
   legend <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
   png(file='plot3.png', width=480, height=480, units='px')
+  
   # Generate plot.
   plot(data[['Calendar_Time']], data[['Sub_metering_1']], type="l", xlab="", ylab="Energy sub metering")
   lines(data[['Calendar_Time']], data[['Sub_metering_2']], type="l", col="red")
